f42bc96ab61065da7df033e1b0727c38e157f7fe
Rob Debbane <studio@robertdebbane.com>
2015-12-17 16:15:55 -0500

    Created module profile.js and moved functions to it

---
+++ profile.js
@@ -0,0 +1,42 @@
+var https = require("https");
+var http = require("http");
+
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
+  console.log(message);
+}
+
+function printError(error) {
+  console.error(error.message);
+}
+
+function get(username) {
+  var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response) {
+    var body = "";
+    response.on("data", function(chunk) {
+      body += chunk;
+    });
+
+    response.on("end", function() {
+      if (response.statusCode === 200) {
+        try {
+          var profile = JSON.parse(body);
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+        } catch(error) {
+          //parsing error
+          printError(error);
+        }
+      } else {
+        //username does not exist error
+        printError({message: "There was a problem getting a profile for username " + username + ". (" + response.statusCode + ": " + http.STATUS_CODES[response.statusCode] + ")"});
+      }
+    });
+
+    //connection error
+    request.on("error", printError);
+  });
+}
+
+
+module.exports.get = get;
+
